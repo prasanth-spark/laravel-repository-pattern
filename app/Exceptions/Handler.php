2407fcf0c9bc9e9879eb3a6b55e396bd81514aa8
prasanth-spark <prasanth@sparkouttech.com>
2021-10-20 12:26:05 +0530

    security vulnerabilities in dependencies

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -34,11 +34,12 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
 
+    
     /**
      * Render an exception into an HTTP response.
      *
@@ -48,8 +49,16 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    public function shouldReport(Throwable $exception){
+        parent::shouldReport($exception);
+
+    }
+    public function renderForConsole($output, Throwable $exception){
+        return parent::renderForConsole($output, $exception);
     }
 }
